55a8aec07eaefbbb98610ab6bf909be2c5008d70
hieunv495 <hieunv495@gmail.com>
2021-01-09 17:18:33 +0700

    Add data request structure documents

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -19,6 +19,7 @@ export const parameters = {
         [
           "Introduction",
           "Install",
+          "Data request structure",
           "Composition",
           "Dashboard",
           "Detail",
